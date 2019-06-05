05a2b66820e84050f762bd115053ce2f8090e9db
gaurav <sharma02gaurav@gmail.com>
2019-06-05 20:30:04 +0530

    removing the files not requird

--- deploy-build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# this contains the deploy build script for lintinte webapp
-
-echo "Building for deployment"
-npm run build
-docker build -t careapp/tutable-webapp:latest .
-docker push careapp/tutable-webapp:latest
