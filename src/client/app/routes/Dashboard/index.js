c9929f3c54b81f0ce3d987d75cb54120980e76c8
Sahil811 <sahilsiddiqui.dev@gmail.com>
2020-10-08 15:33:56 +0530

    convert component from class based to function based

--- src/client/app/routes/Dashboard/index.js
+++ src/client/app/routes/Dashboard/index.js
@@ -1,36 +1,24 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
 import { navigationIndexer } from '../../constants';
 import { switchNavigation } from '../../redux/actions';
 import './index.scss';
 
-class Dashboard extends Component {
 
-	constructor() {
-		super();
-	}
-
-	componentDidUpdate() {
-        const { triggerSwitchNavigation } = this.props;
+const Dashboard = (props) => {
+    useEffect(() => {
+        const { triggerSwitchNavigation } = props;
         document.title = 'Contact Us';
         triggerSwitchNavigation(navigationIndexer.dashboard);
-    }
-
-    componentDidMount() {
-        const { triggerSwitchNavigation } = this.props;
-        document.title = 'Contact Us';
-        triggerSwitchNavigation(navigationIndexer.dashboard);
-    }
-
-	render() {
-		return (
-			<section>
-				<h1>This is the dashboard page.</h1>
-			</section>
-		);
-	}
+    }, [])
+    return (
+        <section>
+            <h1>This is the dashboard page.</h1>
+        </section>
+    );
 }
+
 
 const mapDispatchToProps = dispatch => {
     return {
